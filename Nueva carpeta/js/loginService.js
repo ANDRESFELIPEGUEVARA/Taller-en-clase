c953a19dedecb9cbfb2a516fb9f5007d3cfce3e4
Andres <afguevarac@ufpso.edu.co>
2025-04-30 19:17:40 -0500

    A

--- Nueva carpeta/js/loginService.js
+++ Nueva carpeta/js/loginService.js
@@ -3,37 +3,49 @@ document.getElementById('formLogin').addEventListener('submit', function (e) {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    buscar(email,password)
+    buscar(email, password)
 
-})  
+})
 
-function buscar(email,password){
+function buscar(email, password) {
     let message = ''
     let alertType = ''
     fetch("https://reqres.in/api/login", {
-        method:"POST",
+        method: "POST",
         headers: {
-            "Content-type": "application/json"
+            "Content-type": "application/json",
+            'x-api-key': 'reqres-free-v1'
         },
-        body: JSON.stringify({email,password})
+        body: JSON.stringify({ email, password })
 
     })
 
-    .then( (data)=>{
-        alertType = 'success'
-        message = 'Inicio de sesion exitoso'
-        console.log("Ingreso correcto "+data)
+    .then((response) => {
+        if (response.status === 200) {
+            alertType = 'success'
+            message = 'Inicio de sesion exitoso'
+            console.log("Ingreso correcto " + response)
+            alertBuilder(alertType, message)
+        } else {
+            alertType = 'danger'
+            message = 'correo o contraseña no encontrados'
+            alertBuilder(alertType, message)
+        }
+
     })
-    .catch((error) =>{
+    .catch((error) => {
         alertType = 'danger'
-        message = 'correo o contraseña no encontrados'
+        message = 'Error inesperado'
         console.error(error)
+        alertBuilder(alertType, message)
     })
+}
 
-    let alert = `
+function alertBuilder(alertType, message) {
+    const alert = `
     <div class="alert alert-${alertType} alert-dismissible fade show" role="alert">
         ${message}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>`
-    document.getElementById('alert').innerHTML= alert;
+    document.getElementById('alert').innerHTML = alert;
 }
